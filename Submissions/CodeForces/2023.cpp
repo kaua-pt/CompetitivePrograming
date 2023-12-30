f58df58598ac5abec6c1ce35268f8651c78560d8
Kauã Vinícius <kauaponte@hotmail.com>
2023-12-30 20:40:52 -0300

    Preguiça

---
+++ Submissions/CodeForces/2023.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+    ll h = 1;
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int g;
+        cin >> g;
+        h *= g;
+    }
+
+    int mod = 2023 % h;
+
+    if (mod != 0)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+        solve();
+    return 0;
+}
