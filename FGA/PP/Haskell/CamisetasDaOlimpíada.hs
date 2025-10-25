f6ddd6778e9b55a8b50f1e2c7236608337cef0de
kaua-pt <kauaponte@hotmail.com>
2025-10-25 02:35:50 -0300

    Wrong

--- FGA/PP/Haskell/CamisetasDaOlimpíada.hs
+++ FGA/PP/Haskell/CamisetasDaOlimpíada.hs
@@ -1,3 +1,16 @@
 main :: IO ()
 main = do
-    
+    n <- readLn :: IO Int
+    sizes <- sequence $ replicate n (readLn :: IO Int)
+    p <- readLn :: IO Int
+    m <- readLn :: IO Int
+
+    let (one, two) = foldl
+            (\(ones, others) x ->
+                if x == 1
+                then (ones + 1, others)
+                else (ones, others + 1))
+            (0, 0)
+            sizes
+
+    print (if one <= p && two <= m then 'S' else 'N')
